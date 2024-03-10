c0420cb4836c6872e1d996ad0e214dcc1b10814d
Mitzy Onaolapo <rios_mit@yahoo.com>
2024-03-10 14:11:50 -0700

    Adding 404 page

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const express= require('express'); 
 const app= express(); 
 
@@ -5,6 +7,8 @@ app.get('/', (req, res)=>{
     res.send('Hello World')
 })
 
-app.listen(3000, function(){
-    console.log('We are up and running')
+app.get('*', (req, res)=>{
+    res.status(404).send(`<h1>404 ERROR: Page Not Found</h1>`)
 })
+
+app.listen(process.env.PORT)
