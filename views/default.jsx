4717d0b642ac38e1d91cafe09944c3190a166bcc
Mitzy Onaolapo <rios_mit@yahoo.com>
2024-03-12 21:17:31 -0700

    Adding default view for layout

---
+++ views/default.jsx
@@ -0,0 +1,17 @@
+const react = require('react')
+
+function Def (html) {
+    return (
+        <html>
+            <head>
+                <title>Title</title>
+            </head>
+            <body>
+                {html.children}
+            </body>
+        </html>
+    )
+  }
+  
+
+module.exports = Def
