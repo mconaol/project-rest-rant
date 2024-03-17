75ab2e83c411774d01c43c472b8300f158e9ce61
Mitzy Onaolapo <rios_mit@yahoo.com>
2024-03-17 09:51:24 -0700

    Stopping @P4

--- views/places/new.jsx
+++ views/places/new.jsx
@@ -6,6 +6,21 @@ function new_form(){
         <Def>
             <main>
                 <h1>Add a new Place</h1>
+                <form method="POST" action="/places" style={{ display: 'flex', flexDirection: 'column' }}>
+                    <div>
+                        <label htmlFor="name">Place Name</label>
+                        <input id="name" name="name" style={{ marginBottom: '10px' }}/>
+                        <label htmlFor="pic">Place Picture</label>
+                        <input type="url" id="pic" name="pic" style={{ marginBottom: '10px' }}/>
+                        <label htmlFor="city">Place City</label>
+                        <input id="city" name="city" style={{ marginBottom: '10px' }}/>
+                        <label htmlFor="state">Place State</label>
+                        <input id="state" name="state" style={{ marginBottom: '10px' }}/>
+                        <label htmlFor="cuisines">Place Cuisines</label>
+                        <input id="cuisines" name="cuisines" required style={{ marginBottom: '10px' }}/>
+                    </div>
+                    <input type="submit" value="Add Place"/>
+                </form>
             </main>
         </Def>
     )
