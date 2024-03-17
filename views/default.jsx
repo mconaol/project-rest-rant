a919cce6085a9e36312ec9a3dfc8fe90a5a4532f
Mitzy Onaolapo <rios_mit@yahoo.com>
2024-03-17 13:27:54 -0700

    Show route connected

--- views/default.jsx
+++ views/default.jsx
@@ -6,8 +6,22 @@ function Def (html) {
             <head>
                 <title>Title</title>
                 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossOrigin="anonymous"/>
+                <link rel="stylesheet" href="/style.css" />
             </head>
             <body>
+                <nav>
+                    <ul>
+                        <li>
+                            <a href="/">Home</a>
+                        </li>
+                        <li>
+                        <a href="/places">Places</a>
+                        </li>
+                        <li>
+                        <a href="/places/new">Add Place</a>
+                        </li>
+                    </ul>
+                </nav>
                 {html.children}
             </body>
         </html>
--- views/home.jsx
+++ views/home.jsx
@@ -7,7 +7,7 @@ function home () {
           <main>
               <h1>REST-Rant</h1>
               <div>
-                <img src="/images/tuna_poke_bowl.jpg" alt="Tuna poke bowl" />
+                <img src="../images/tuna_poke_bowl.jpg" alt="Tuna poke bowl" width= "600" height="600" />
                 <div>
                   Photo by <a href="https://unsplash.com/@sheri_silver?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">sheri silver</a> on <a href="https://unsplash.com/photos/two-bowls-of-food-are-sitting-on-a-table--HFD24ee1LQ?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
                 </div>
